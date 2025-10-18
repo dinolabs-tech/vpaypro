76e0d4d364fada94f7ac6aa410299db240eef5c9
DinoLabs Tech Services <olaoluwadaniel1091@gmail.com>
2025-10-18 21:32:00 +0100

    aa

--- backend/index.php
+++ backend/index.php
@@ -15,7 +15,7 @@
             <div class="d-flex justify-content-center py-4">
               <a href="index.php" class="logo d-flex align-items-center w-auto">
                 <!-- <img src="assets/img/logo.png" alt=""> -->
-                <span class="d-none d-lg-block text-dark">VPayPro</span>
+                <span class="d-none d-lg-block text-dark">VPayPro AA</span>
               </a>
             </div> <!-- End Logo -->
 
