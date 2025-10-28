f9ed35a49d23ec5eb3aa5ee986d0e184354cfa5b
DinoLabs Tech Services <olaoluwadaniel1091@gmail.com>
2025-10-28 12:30:25 +0100

    removed the session role called initially

--- backend/developer.php
+++ backend/developer.php
@@ -94,7 +94,7 @@ if (isset($_GET['action']) && $_GET['action'] === 'clear_log' && isset($_GET['lo
                 <div class="page-inner">
                     <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
                         <div>
-                            <h3 class="fw-bold mb-3">Developer <?= $_SESSION['role']?></h3>
+                            <h3 class="fw-bold mb-3">Developer</h3>
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="superdashboard.php">Home</a></li>
                                 <li class="breadcrumb-item active">Developer</li>
